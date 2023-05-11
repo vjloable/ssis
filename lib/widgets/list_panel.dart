a37749d59a1f9b69c8e4f5dc4887970716e4f1f1
Eagl3ye <vincejaphethloable@gmail.com>
2023-05-11 14:38:56 +0800

    feat: add custom widget ListPanel

---
+++ lib/widgets/list_panel.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class ListPanel extends StatelessWidget {
+  final Color headerColor;
+  final double headHeight;
+  final double? width;
+  final Color bodyColor;
+  final double bodyHeight;
+  final double borderRadius;
+  final Widget? child;
+  final double elevation;
+
+  const ListPanel({
+    Key? key,
+    this.headHeight = 44.0,
+    this.bodyHeight = 44.0,
+    this.width,
+    this.child,
+    this.borderRadius = 0.0,
+    this.headerColor = Colors.grey,
+    this.bodyColor = Colors.white,
+    this.elevation = 0.0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Material(
+          elevation: elevation,
+          color: headerColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(topRight: Radius.circular(borderRadius), topLeft: Radius.circular(borderRadius)),
+          ),
+          child: SizedBox(
+            height: headHeight,
+            width: width
+          ),
+        ),
+        Material(
+          elevation: elevation,
+          color: bodyColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(bottomRight: Radius.circular(borderRadius), bottomLeft: Radius.circular(borderRadius)),
+          ),
+          child: SizedBox(
+            height: bodyHeight,
+            width: width,
+            child: child
+          ),
+        ),
+      ],
+    );
+  }
+}
