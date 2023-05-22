c1aff8e9f68b825a961d49dad4da6008f9a25792
Eagl3ye <vincejaphethloable@gmail.com>
2023-05-22 18:47:54 +0800

    refactor: transfer formattedCourseList method to the CourseHandler class

--- lib/handlers/course_handler.dart
+++ lib/handlers/course_handler.dart
@@ -7,4 +7,20 @@ class CourseHandler{
     }
     return returnString;
   }
+
+  Future<List<String>> formattedCoursesList(Future<List<List<dynamic>>> futureList) async {
+    List<List<dynamic>> rawList = await futureList;
+    List<String> filteredList = [];
+    if(rawList.length > 1){
+      for (var row in rawList) {
+        if(row.elementAt(0) != "BachType") {
+          filteredList.add("B${row.elementAt(0).toString().substring(0,1)} ${row.elementAt(1).toString()}");
+        }
+      }
+    }else{
+      filteredList = [''];
+    }
+    return filteredList.toSet().toList();
+  }
+
 }
