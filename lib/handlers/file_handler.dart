572a9184c7c48ab219d7f600313829960d86c212
Eagl3ye <vincejaphethloable@gmail.com>
2023-05-24 21:04:52 +0800

    feat: add appendData method for the FileHandler class
    fix: resurface file existence checker to initData method of FileHandler class

--- lib/handlers/file_handler.dart
+++ lib/handlers/file_handler.dart
@@ -1,33 +1,10 @@
 import 'dart:convert';
 import 'dart:io';
-
-import 'package:path_provider/path_provider.dart';
+import 'package:flutter/foundation.dart';
 import 'package:csv/csv.dart';
 
 class FileHandler{
-  void initCSVFile(List<List<String>> data, String filename) async{
-    String csvData = const ListToCsvConverter().convert(data);
-    // String directory = (await getApplicationSupportDirectory()).path;
-    Directory dir = Directory.fromUri(Uri.directory('userdata'));
-    dir.createSync(recursive: true);
-    File file = File('${dir.absolute.path}$filename.csv');
-    print('initializing: ${file.absolute.path}');
-    file.writeAsStringSync(csvData);
-    // if(File(filePath).existsSync()){
-    //   if (kDebugMode) {
-    //     print('The specified file in the $filePath already exists');
-    //   }
-    // }else{
-    //   if (kDebugMode) {
-    //     print('The specified file in the $filePath does not yet exists');
-    //   }
-    //   File file = File(filePath);
-    //   await file.writeAsString(csvData);
-    // }
-  }
-
   Future<List<List<dynamic>>> loadCSVFile(String filename) async {
-    // String directory = (await getApplicationDocumentsDirectory()).path;
     Directory dir = Directory.fromUri(Uri.directory('userdata'));
     dir.createSync(recursive: true);
     File file = File('${dir.absolute.path}$filename.csv');
@@ -35,12 +12,30 @@ class FileHandler{
     return await file.openRead().transform(utf8.decoder).transform(const CsvToListConverter()).toList();
   }
 
-  insertData(List<List<String>> data, String filename) async{
+  void initData(List<List<String>> data, String filename) async{
     String csvData = const ListToCsvConverter().convert(data);
-    String directory = (await getApplicationDocumentsDirectory()).path;
-    String filePath = "$directory\\$filename.csv";
-    File file = File(filePath);
-    await file.writeAsString(csvData);
+    Directory dir = Directory.fromUri(Uri.directory('userdata'));
+    dir.createSync(recursive: true);
+    File file = File('${dir.absolute.path}$filename.csv');
+    if(file.existsSync()){
+      if (kDebugMode) {
+        print('The specified file in the ${file.absolute.path} already exists');
+      }
+    }else{
+      if (kDebugMode) {
+        print('initializing: ${file.absolute.path}');
+      }
+      file.writeAsStringSync(csvData);
+    }
+  }
+
+  void appendData(List<List<String>> data, String filename) async{
+    String csvData = const ListToCsvConverter().convert(data);
+    Directory dir = Directory.fromUri(Uri.directory('userdata'));
+    dir.createSync(recursive: true);
+    File file = File('${dir.absolute.path}$filename.csv');
+    print('appending: ${file.absolute.path}');
+    file.writeAsStringSync(csvData);
   }
 
   editData(List<List<String>> data) async{
