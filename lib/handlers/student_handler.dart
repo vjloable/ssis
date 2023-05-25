79b5386088ae91aa57caa985f377f5eae41136af
Eagl3ye <vincejaphethloable@gmail.com>
2023-05-25 08:45:30 +0800

    feat: add submit method, validators, getters, and setters for student details handling by the StudentHandler class

--- lib/handlers/student_handler.dart
+++ lib/handlers/student_handler.dart
@@ -1,7 +1,92 @@
+import 'package:ssis/repositories/student_repository.dart';
+
 class StudentHandler{
+  String? _validatorIDNum;
+  String? _validatorFName;
+  String? _validatorMInitial;
+  String? _validatorLName;
+  String? _validatorAge;
+  String? _validatorSex;
+  String? _validatorCourse;
+  String? _validatorYearLevel;
+
+  String? getAge(){
+    return _validatorAge;
+  }
+
+  String? getSex(){
+    return _validatorSex;
+  }
+
+  String? getCourse(){
+    return _validatorCourse;
+  }
+
+  String? getYearLevel(){
+    return _validatorYearLevel;
+  }
+
+  void addIDNum(String? idNum){
+    _validatorIDNum = idNum;
+  }
+
+  void addFName(String? firstName){
+    _validatorFName = firstName;
+  }
+
+  void addMInitial(String? middleInitial){
+    _validatorMInitial = middleInitial;
+  }
+
+  void addLName(String? lastName){
+    _validatorLName = lastName;
+  }
+
+  void addAge(String? age){
+    _validatorAge = age;
+  }
+
+  void addSex(String? sex){
+    _validatorSex = sex;
+  }
+
+  void addCourse(String? course){
+    _validatorCourse = course;
+  }
+
+  void addYearLevel(String? yearLevel){
+    _validatorYearLevel = yearLevel;
+  }
+
+  void submit(){
+    StudentRepository studentRepository = StudentRepository();
+    if(_validatorIDNum != null && _validatorFName != null && _validatorMInitial != null && _validatorLName != null && _validatorAge != null && _validatorSex != null && _validatorCourse != null && _validatorYearLevel != null){
+      if(_validatorIDNum.toString().trim() != '' && _validatorFName.toString().trim() != '' && _validatorMInitial.toString().trim() != '' && _validatorLName.toString().trim() != ''){
+        studentRepository.add(
+          _validatorIDNum.toString(),
+          _validatorFName.toString(),
+          _validatorMInitial.toString(),
+          _validatorLName.toString(),
+          _validatorAge.toString(),
+          _validatorSex.toString(),
+          _validatorCourse.toString(),
+          _validatorYearLevel.toString(),
+        );
+      }else{
+        print("Empty fields");
+      }
+    }else{
+      print("Null values found");
+    }
+  }
+
   String formatterFullName(List<dynamic> data, String header){
     String returnString = header;
-    if(data.elementAt(1) != 'FirstN'){
+    if(data.elementAt(1) == '-'){
+      returnString = '-';
+    }else if(data.elementAt(1) == 'FirstN'){
+      returnString = header;
+    }else{
       returnString = '${data.elementAt(1).toString().trim()} ${data.elementAt(2).toString().trim()} ${data.elementAt(3).toString().trim()}';
     }
     return returnString;
