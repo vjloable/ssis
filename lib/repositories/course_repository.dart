057d7a3f32886b99a0349388a188a869772177f4
Eagl3ye <vincejaphethloable@gmail.com>
2023-05-25 08:37:42 +0800

    feat: add an add method for CourseRepository class

--- lib/repositories/course_repository.dart
+++ lib/repositories/course_repository.dart
@@ -4,29 +4,10 @@ class CourseRepository{
   void init() async{
     FileHandler handler = FileHandler();
     List<List<String>> coursesData = [
-      ["BachType", "Course", "College"],
-      ["Arts", "Psychology", "CASS"],
-      ["Science", "Psychology", "CHS"],
-      ["Science", "Statistics", "CSM"],
-      ["Science", "Computer Science", "CCS"],
-      ["Arts", "Psychology", "CASS"],
-      ["Science", "Psychology", "CHS"],
-      ["Science", "Statistics", "CSM"],
-      ["Science", "Computer Science", "CCS"],
-      ["Arts", "Psychology", "CASS"],
-      ["Science", "Psychology", "CHS"],
-      ["Science", "Statistics", "CSM"],
-      ["Science", "Computer Science", "CCS"],
-      ["Arts", "Psychology", "CASS"],
-      ["Science", "Psychology", "CHS"],
-      ["Science", "Statistics", "CSM"],
-      ["Science", "Computer Science", "CCS"],
-      ["Arts", "Psychology", "CASS"],
-      ["Science", "Psychology", "CHS"],
-      ["Science", "Statistics", "CSM"],
-      ["Science", "Computer Science", "CCS"],
+      ["BachType", "Course"],
+      // ["Science", "Computer Science"],
     ];
-    handler.initCSVFile(coursesData, "courses");
+    handler.initData(coursesData, "courses");
     print('Course Service initialized!');
   }
   
@@ -35,30 +16,56 @@ class CourseRepository{
     return handler.loadCSVFile("courses");
   }
 
-  // void add(String batchType, String course, String college) async{
-  //   List<List<dynamic>> dataList = await getList().then((data) => data);
-  //   List<dynamic> dataHead = dataList.first;
-  //   if (dataList.length > 1){
-  //     int index = 0;
-  //     for (final dataItem in dataList){
-  //       String courseSrc = course.toString().trim().toLowerCase();
-  //       String batchTypeSrc = batchType.toString().trim().toLowerCase();
-  //       String collegeSrc = college.toString().trim().toLowerCase();
-  //       String courseDes = dataItem.elementAt(1).toString().trim().toLowerCase();
-  //       String batchTypeDes = dataItem.elementAt(0).toString().trim().toLowerCase();
-  //       String collegeDes = dataItem.elementAt(2).toString().trim().toLowerCase();
-  //       if (courseSrc == courseDes){
-  //         // if ()
-  //         break;
-  //       }else{
-  //         index++;
-  //       }
-  //     }
-  //     print('Course already exists.');
-  //   }else{
-  //     FileHandler handler = FileHandler();
-  //     List<List<String>> coursesData = [];
-  //     handler.initCSVFile(coursesData, "courses");
-  //   }
-  // }
+  void add(String batchType, String course) async{
+    List<List<dynamic>> dataList = await getList();
+    // List<dynamic> dataHead = dataList.first;
+    List<List<String>> coursesData = [];
+    bool passed = false;
+    int x = 0;
+    if (dataList.length > 1){
+      int index = 0;
+      for (final dataItem in dataList){
+        String courseSrc = course.toString().trim().toLowerCase();
+        String batchTypeSrc = batchType.toString().trim().toLowerCase();
+        String courseDes = dataItem.elementAt(1).toString().trim().toLowerCase();
+        String batchTypeDes = dataItem.elementAt(0).toString().trim().toLowerCase();
+        if(index > 0){
+          if (courseSrc == courseDes){
+            if (batchTypeSrc != batchTypeDes) {
+              passed = true;
+              x = 2;
+            }else{
+              passed = false;
+              x = 100;
+              break;
+            }
+          }else{
+            passed = true;
+            x = 3;
+          }
+        }
+        index++;
+      }
+    }else{
+      passed = true;
+      x = 4;
+      print('Appended Bachelor of $batchType in $course');
+    }
+    if(passed){
+      FileHandler handler = FileHandler();
+      for (final dataItem in dataList) {
+        coursesData.add([
+          dataItem.elementAt(0).toString(),
+          dataItem.elementAt(1).toString(),
+        ]);
+      }
+      coursesData.add([batchType.trim(),course.trim()]);
+      handler.appendData(coursesData, "courses");
+      print('Appended Bachelor of $batchType in $course');
+      print(x);
+    }else{
+      print('Course already exists.');
+      print(x);
+    }
+  }
 }
