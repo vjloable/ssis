cbd74af6d5a51c5c5b899092ff3a2af98423aa54
Eagl3ye <vincejaphethloable@gmail.com>
2023-06-04 18:17:17 +0800

    refactor: modify edit, add, and delete methods of the CourseRepository

--- lib/repositories/course_repository.dart
+++ lib/repositories/course_repository.dart
@@ -4,8 +4,7 @@ class CourseRepository{
   void init() async{
     FileHandler handler = FileHandler();
     List<List<String>> coursesData = [
-      ["BachType", "Course"],
-      // ["Science", "Computer Science"],
+      ["CourseCode", "Course"],
     ];
     handler.initData(coursesData, "courses");
     print('Course Service initialized!');
@@ -13,12 +12,13 @@ class CourseRepository{
   
   Future<List<List<dynamic>>> getList() async {
     FileHandler handler = FileHandler();
+    print('getlist: ${ await handler.loadCSVFile("courses")}');
     return handler.loadCSVFile("courses");
   }
 
-  void add(String batchType, String course) async{
+  Future<bool> add(String courseCode, String course) async{
+    bool isSuccess = false;
     List<List<dynamic>> dataList = await getList();
-    // List<dynamic> dataHead = dataList.first;
     List<List<String>> coursesData = [];
     bool passed = false;
     int x = 0;
@@ -26,12 +26,12 @@ class CourseRepository{
       int index = 0;
       for (final dataItem in dataList){
         String courseSrc = course.toString().trim().toLowerCase();
-        String batchTypeSrc = batchType.toString().trim().toLowerCase();
+        String courseCodeSrc = courseCode.toString().trim().toLowerCase();
         String courseDes = dataItem.elementAt(1).toString().trim().toLowerCase();
-        String batchTypeDes = dataItem.elementAt(0).toString().trim().toLowerCase();
+        String courseCodeDes = dataItem.elementAt(0).toString().trim().toLowerCase();
         if(index > 0){
           if (courseSrc == courseDes){
-            if (batchTypeSrc != batchTypeDes) {
+            if (courseCodeSrc != courseCodeDes) {
               passed = true;
               x = 2;
             }else{
@@ -49,7 +49,6 @@ class CourseRepository{
     }else{
       passed = true;
       x = 4;
-      print('Appended Bachelor of $batchType in $course');
     }
     if(passed){
       FileHandler handler = FileHandler();
@@ -59,13 +58,77 @@ class CourseRepository{
           dataItem.elementAt(1).toString(),
         ]);
       }
-      coursesData.add([batchType.trim(),course.trim()]);
+      coursesData.add([courseCode.trim(),course.trim()]);
       handler.appendData(coursesData, "courses");
-      print('Appended Bachelor of $batchType in $course');
+      isSuccess = true;
+      print('Appended [$courseCode] $course');
       print(x);
     }else{
+      isSuccess = false;
       print('Course already exists.');
       print(x);
     }
+    return isSuccess;
+  }
+
+  Future<bool> edit(String prevCourseCode, String courseCode, String course) async{
+    bool isSuccess = false;
+    List<List<dynamic>> dataList = await getList();
+    List<List<String>> coursesData = [];
+    bool passed = false;
+    int index = 0;
+    if (dataList.length > 1){
+      for (final dataItem in dataList){
+        String courseCodeSrc = prevCourseCode;
+        String courseCodeDes = dataItem.elementAt(0).toString().trim();
+        if(index > 0){
+          print('src: $courseCodeSrc, des: $courseCodeDes');
+          if (courseCodeSrc == courseCodeDes){
+            passed = true;
+            break;
+          }else{
+            passed = false;
+          }
+        }
+        index++;
+      }
+    }
+    if(passed){
+      FileHandler handler = FileHandler();
+      for (final dataItem in dataList) {
+        coursesData.add([
+          dataItem.elementAt(0).toString(),
+          dataItem.elementAt(1).toString(),
+        ]);
+      }
+      coursesData.insert(index, [courseCode.trim(), course.trim()]);
+      coursesData.removeAt(index+1);
+      handler.appendData(coursesData, "courses");
+      isSuccess = true;
+      print('Edited course of code ${courseCode.trim()} to ${course.trim()}');
+    }else{
+      isSuccess = false;
+      print('Unable to edit.');
+    }
+    return isSuccess;
+  }
+
+  Future<bool> delete(List<List<dynamic>> listData) async {
+    bool isSuccess = false;
+    List<List<dynamic>> dataList = await getList();
+    List<List<String>> coursesData = [];
+    for (final listItem in listData) {
+      dataList.removeWhere((element) => element.first == listItem.first);
+    }
+    FileHandler handler = FileHandler();
+    for (final dataItem in dataList) {
+      coursesData.add([
+        dataItem.elementAt(0).toString(),
+        dataItem.elementAt(1).toString(),
+      ]);
+    }
+    handler.appendData(coursesData, "courses");
+    isSuccess = true;
+    return isSuccess;
   }
 }
