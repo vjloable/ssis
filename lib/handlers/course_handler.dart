38ba58c141ec1cf3cfe60d615e9d92001acdd680
Eagl3ye <vincejaphethloable@gmail.com>
2023-06-22 08:39:18 +0800

    refactor: change add, edit, and delete methods to handle user inputs for courses

--- lib/handlers/course_handler.dart
+++ lib/handlers/course_handler.dart
@@ -1,4 +1,5 @@
 import 'package:ssis/repositories/course_repository.dart';
+import 'package:ssis/models/course_model.dart';
 
 class CourseHandler{
   String? _validatorCourseCode;
@@ -18,7 +19,7 @@ class CourseHandler{
   }
 
   Future<bool> submitAdd() async {
-    bool isSuccess = false;
+    bool isSuccess = true;
     CourseRepository courseRepository = CourseRepository();
     if(_validatorCourseCode != null && _validatorCourse != null){
       if(_validatorCourse.toString().trim() != ''){
@@ -37,11 +38,8 @@ class CourseHandler{
     CourseRepository courseRepository = CourseRepository();
     if(_validatorCourseCode != null && _validatorCourse != null){
       if(_validatorCourseCode.toString().trim() != '' && _validatorCourse.toString().trim() != ''){
-        isSuccess = await courseRepository.edit(
-            prevCourseCode.trim(),
-            _validatorCourseCode.toString(),
-            _validatorCourse.toString()
-        );
+        CourseModel newCourse = CourseModel(courseCode: _validatorCourseCode.toString().trim(), course: _validatorCourse.toString().trim());
+        isSuccess = await courseRepository.update(prevCourseCode, newCourse);
       }else{
         isSuccess = false;
       }
@@ -51,44 +49,61 @@ class CourseHandler{
     return isSuccess;
   }
 
-  Future<bool> submitDelete(List<List<dynamic>> listData) async {
+  Future<bool> submitDelete(List<dynamic> listData) async {
     bool isSuccess = false;
     CourseRepository courseRepository = CourseRepository();
-    isSuccess = await courseRepository.delete(listData);
+    List<CourseModel> listCourses = listData.map((e) => e as CourseModel).toList();
+    isSuccess = await courseRepository.delete(listCourses);
     return isSuccess;
   }
 
-  String formatter(List<dynamic> data, String header, int index){
-    String returnString = header;
-    if(data.elementAt(index) != comparisonList[index]){
-      returnString = data.elementAt(index).toString().trim();
+  // Future<bool> submitEdit(String prevCourseCode) async {
+  //   bool isSuccess = false;
+  //   CourseRepository courseRepository = CourseRepository();
+  //   if(_validatorCourseCode != null && _validatorCourse != null){
+  //     if(_validatorCourseCode.toString().trim() != '' && _validatorCourse.toString().trim() != ''){
+  //       isSuccess = await courseRepository.edit(
+  //           prevCourseCode.trim(),
+  //           _validatorCourseCode.toString(),
+  //           _validatorCourse.toString()
+  //       );
+  //     }else{
+  //       isSuccess = false;
+  //     }
+  //   }else{
+  //     isSuccess = false;
+  //   }
+  //   return isSuccess;
+  // }
+
+  // String formatter(List<dynamic> data, String header, int index){
+  //   String returnString = header;
+  //   if(data.elementAt(index) != comparisonList[index]){
+  //     returnString = data.elementAt(index).toString().trim();
+  //   }
+  //   return returnString;
+  // }
+
+  Future<List<String>> formattedCoursesMap(List<CourseModel> listCourses) async {
+    List<String> formattedListCourses = [];
+    if(listCourses.length > 1){
+      for(var i = 1 ; i < listCourses.length; i++) {
+        formattedListCourses.add(listCourses[i].courseCode);
+      }
     }
-    return returnString;
+    return formattedListCourses;
   }
 
-  Future<Map<String,String>> formattedCoursesMap(Future<List<List<dynamic>>> futureList) async {
-    List<List<dynamic>> rawList = await futureList;
-    Map<String, String> filteredMap = {};
-    if(rawList.length > 1){
-      for(var i = 1 ; i < rawList.length; i++) {
-        filteredMap[rawList.elementAt(i).elementAt(0)] = rawList.elementAt(i).elementAt(1);
-      }
-    }else{
-      filteredMap = {};
-    }
-    return filteredMap;
-  }
-
-  Future<Map<String, String>> formattedCoursesMapInverted(Future<List<List<dynamic>>> futureList) async {
-    List<List<dynamic>> rawList = await futureList;
-    Map<String, String> filteredMap = {};
-    if(rawList.length > 1){
-      for(var i = 1 ; i < rawList.length; i++) {
-        filteredMap[rawList.elementAt(i).elementAt(1)] = rawList.elementAt(i).elementAt(0);
-      }
-    }else{
-      filteredMap = {};
-    }
-    return filteredMap;
-  }
+  // Future<Map<String, String>> formattedCoursesMapInverted(Future<List<List<dynamic>>> futureList) async {
+  //   List<List<dynamic>> rawList = await futureList;
+  //   Map<String, String> filteredMap = {};
+  //   if(rawList.length > 1){
+  //     for(var i = 1 ; i < rawList.length; i++) {
+  //       filteredMap[rawList.elementAt(i).elementAt(1)] = rawList.elementAt(i).elementAt(0);
+  //     }
+  //   }else{
+  //     filteredMap = {};
+  //   }
+  //   return filteredMap;
+  // }
 }
